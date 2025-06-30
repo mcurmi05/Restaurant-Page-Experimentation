80c4a213f9138f777a028fbeaf076d51eafe31e2
mcurmi05 <mcurmi05@gmail.com>
2025-06-30 13:01:44 +1000

    Improvements to the user interface, now time to start javascripting

---
+++ src/home.js
@@ -0,0 +1,8 @@
+import "./styles.css"
+
+const menuBtn = document.querySelector("#menuBtn");
+const locationsBtn = document.querySelector("#locationsBtn");
+const galleryBtn = document.querySelector("#galleryBtn")
+const orderBtn = document.querySelector("#orderBtn")
+
+menuBtn.addEventListener()
